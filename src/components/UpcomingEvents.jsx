0b011fe4d449392f93ffe63d01b814efed3fa6c8
Nafis5115 <72686804+Nafis5115@users.noreply.github.com>
2026-01-20 11:59:34 +0600

    integrated upcoming-event api

--- src/components/UpcomingEvents.jsx
+++ src/components/UpcomingEvents.jsx
@@ -1,7 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router";
+import EventCard from "./cards/EventCard";
+import useAxios from "../hooks/useAxios";
 
 const UpcomingEvents = () => {
+  const [events, setEvents] = useState([]);
+  const axios = useAxios();
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    (async () => {
+      try {
+        setLoading(true);
+        const res = await axios.get("/upcoming-events");
+        setEvents(res.data);
+        setLoading(false);
+      } catch (error) {
+        console.log(error);
+      }
+    })();
+  }, [axios]);
+
+  if (loading)
+    return (
+      <div className=" flex justify-center w-full items-center h-screen">
+        <span className="loading loading-spinner text-black loading-sm"></span>
+      </div>
+    );
   return (
     <div>
       <section className="py-24 bg-soft text-black">
@@ -11,77 +35,9 @@ const UpcomingEvents = () => {
           </h3>
 
           <div className="grid md:grid-cols-3 gap-8">
-            <div className="bg-white rounded-xl shadow hover:shadow-lg transition overflow-hidden">
-              <img
-                src="https://images.unsplash.com/photo-1554224155-8d04cb21cd6c"
-                className="h-44 w-full object-cover"
-              />
-              <div className="p-6">
-                <span className="text-xs bg-red text-white px-3 py-1 rounded-full">
-                  Cleanup
-                </span>
-                <h4 className="font-bold text-lg mt-3">
-                  Community Cleanup – Uttara Sector 7
-                </h4>
-                <p className="text-sm text-gray-600 mt-1">
-                  📍 Dhaka • 🗓 12 March 2026
-                </p>
-                <a
-                  href="event-details.html"
-                  className="inline-block mt-4 text-red font-semibold hover:underline"
-                >
-                  View Details →
-                </a>
-              </div>
-            </div>
-
-            <div className="bg-white rounded-xl shadow hover:shadow-lg transition overflow-hidden">
-              <img
-                src="https://images.unsplash.com/photo-1469474968028-56623f02e42e"
-                className="h-44 w-full object-cover"
-              />
-              <div className="p-6">
-                <span className="text-xs bg-rose text-white px-3 py-1 rounded-full">
-                  Plantation
-                </span>
-                <h4 className="font-bold text-lg mt-3">
-                  Tree Plantation – Gazipur Highway
-                </h4>
-                <p className="text-sm text-gray-600 mt-1">
-                  📍 Gazipur • 🗓 20 March 2026
-                </p>
-                <a
-                  href="event-details.html"
-                  className="inline-block mt-4 text-red font-semibold hover:underline"
-                >
-                  View Details →
-                </a>
-              </div>
-            </div>
-
-            <div className="bg-white rounded-xl shadow hover:shadow-lg transition overflow-hidden">
-              <img
-                src="https://images.unsplash.com/photo-1526256262350-7da7584cf5eb"
-                className="h-44 w-full object-cover"
-              />
-              <div className="p-6">
-                <span className="text-xs bg-maroon text-white px-3 py-1 rounded-full">
-                  Donation
-                </span>
-                <h4 className="font-bold text-lg mt-3">
-                  Ramadan Food Distribution Program
-                </h4>
-                <p className="text-sm text-gray-600 mt-1">
-                  📍 Sylhet • 🗓 28 March 2026
-                </p>
-                <a
-                  href="event-details.html"
-                  className="inline-block mt-4 text-red font-semibold hover:underline"
-                >
-                  View Details →
-                </a>
-              </div>
-            </div>
+            {events.map((event) => (
+              <EventCard key={event._id} event={event}></EventCard>
+            ))}
           </div>
 
           <div className="text-center mt-14 cursor-pointer">
---
+++ src/components/cards/EventCard.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const EventCard = ({ event }) => {
+  return (
+    <div className="bg-white rounded-xl shadow hover:shadow-lg transition overflow-hidden">
+      <img src={event.thumbnailUrl} className="h-44 w-full object-cover" />
+      <div className="p-6">
+        <span className="text-xs bg-red text-white px-3 py-1 rounded-full">
+          {event.eventType}
+        </span>
+        <h4 className="font-bold text-lg mt-3">{event.eventTitle}</h4>
+        <p className="text-sm text-gray-600 mt-1">
+          📍 {event.location} • 🗓 {event.startDate}
+        </p>
+        <a
+          href="event-details.html"
+          className="inline-block mt-4 text-red font-semibold cursor-pointer"
+        >
+          View Details →
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default EventCard;
